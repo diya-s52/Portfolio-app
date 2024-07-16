b98b6c15d9266ad216a978c5ddb2d4c559553085
diya-s52 <diyasharma65180@gmail.com>
2024-07-16 12:28:49 +0530

    first commit

---
+++ src/containers/portfolio/index.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import PageHeaderContent from "../../components/pageHeaderContent";
+import { BsInfoCircleFill } from "react-icons/bs";
+import ImageOne from "../../images/image1.png";
+
+import "./styles.scss";
+import { useState } from "react";
+
+const portfolioData = [
+  {
+    id: 2,
+    name: "PG Finding Website",
+    image: ImageOne,
+    link: "",
+  }
+ 
+];
+
+const filterData = [
+  {
+    filterId: 1,
+    label: "All",
+  },
+  {
+    filterId: 2,
+    label: "Developement",
+  },
+  {
+    filterId: 3,
+    label: "Design",
+  },
+];
+
+const Portfolio = () => {
+  const [filteredvalue, setFilteredValue] = useState(1);
+  const [hoveredValue, setHoveredValue] = useState(null);
+
+  function handleFilter(currentId) {
+    setFilteredValue(currentId);
+  }
+
+  function handleHover(index) {
+    setHoveredValue(index);
+  }
+
+  console.log("====================================");
+  console.log(hoveredValue);
+  console.log("====================================");
+
+  const filteredItems =
+    filteredvalue === 1
+      ? portfolioData
+      : portfolioData.filter((item) => item.id === filteredvalue);
+
+  console.log(filteredItems);
+
+  return (
+    <section id="portfolio" className="portfolio">
+      <PageHeaderContent
+        headerText="My Portfolio"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="portfolio__content">
+        <ul className="portfolio__content__filter">
+          {filterData.map((item) => (
+            <li
+              className={item.filterId === filteredvalue ? "active" : ""}
+              onClick={() => handleFilter(item.filterId)}
+              key={item.filterId}
+            >
+              {item.label}
+            </li>
+          ))}
+        </ul>
+        <div className="portfolio__content__cards">
+          {filteredItems.map((item, index) => (
+            <div
+              className="portfolio__content__cards__item"
+              key={`cardItem${item.name.trim()}`}
+              onMouseEnter={() => handleHover(index)}
+              onMouseLeave={() => handleHover(null)}
+            >
+              <div className="portfolio__content__cards__item__img-wrapper">
+                <a>
+                  <img alt="dummy data" src={item.image} />
+                </a>
+              </div>
+              <div className="overlay">
+                {index === hoveredValue && (
+                  <div>
+                    <p>{item.name}</p>
+                    <button>Visit</button>
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Portfolio;
---
+++ src/containers/resume/utils.js
@@ -0,0 +1,32 @@
+export const data = {
+    experience: [
+      {
+        title: "Vice President",
+        subTitle: "of   Fine Arts Society of College",
+        description:
+          " Leadership Experience, Management Skills",
+         
+      },
+      
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "Vidya Jain Public School",
+        description:
+          "10th Grade",
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "Vidya Jain Public School",
+        description:
+          "12th Grade in Science",
+      },
+      {
+        title: "B.Tech",
+        subTitle: "Dr. Akhilesh Das Gupta Institute of Professional Studies",
+        description:
+          "Bachelor of technology in Information Technology",
+      },
+    ],
+  };
